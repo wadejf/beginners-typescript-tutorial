7d9d829f88671f6d34b6c057cde482fd69f27463
Facundo Wade <wadefacundo@gmail.com>
2023-03-07 23:33:02 -0300

    Finished exercise 13

--- src/13-catch-blocks.problem.ts
+++ src/13-catch-blocks.problem.ts
@@ -6,7 +6,8 @@ const tryCatchDemo = (state: "fail" | "succeed") => {
       throw new Error("Failure!");
     }
   } catch (e) {
-    return e.message;
+    if (e instanceof Error)
+      return e.message;
   }
 };
 
